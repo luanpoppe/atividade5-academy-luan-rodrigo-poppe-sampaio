2a77c282fc5a0fea584420a291eab381d89c8c8d
luanpoppe <luanpoppe@gmail.com>
2024-04-27 09:39:50 -0300

    feat: criando page objects da página inicial

---
+++ cypress/support/pages/pagina-inicial.js
@@ -0,0 +1,8 @@
+export class paginaInicial {
+    buttonNextPage = "#paginacaoProximo"
+    buttonPreviousPage = "#paginacaoVoltar"
+    buttonPreviousPage = "#paginacaoVoltar"
+    buttonNovoUsuario = "[href='/users/novo']"
+
+    inputPesquisar = "[placeholder='E-mail ou nome']"
+}
