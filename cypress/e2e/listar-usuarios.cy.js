341cea3ff5ed09f707d50a393daa1d8174389f97
luanpoppe <luanpoppe@gmail.com>
2024-04-27 11:42:47 -0300

    feat: adicionando testes de listar usuários

--- cypress/e2e/listar-usuarios.cy.js
+++ cypress/e2e/listar-usuarios.cy.js
@@ -1,16 +1,91 @@
+import { PaginaInicial } from "../support/pages/pagina-inicial"
+
 describe('template spec', () => {
+  const paginaInicial = new PaginaInicial()
   beforeEach(function () {
     cy.viewport("macbook-13")
     cy.visit('/')
   })
 
-  it('passes', () => {
-    cy.intercept("GET", "api/v1/users", {
+  describe('Recebendo resposta com 06 usuários', function () {
+    beforeEach(function () {
+      cy.intercept("GET", "api/v1/users", {
+        fixture: "get-usuarios/get-6-usuarios.json"
+      }).as("getUsers")
 
-    }).as("getUsers")
+      cy.wait("@getUsers")
+    })
 
-    cy.wait("@getUsers").then((resposta) => {
-      cy.log(resposta)
+    it('Total de páginas deve ser um', () => {
+      cy.get(paginaInicial.itemPaginacaoAtual).should("have.text", "1 de 1")
+    })
+
+    it('Botões de avançar e voltar página devem estar desabilitados', function () {
+      cy.get(paginaInicial.buttonNextPage).should("be.disabled")
+      cy.get(paginaInicial.buttonPreviousPage).should("be.disabled")
+    })
+
+    it('Total de usuários mostrados na página deve ser até 06', function () {
+      cy.get(`${paginaInicial.divListaUsuarios} > div`).should("have.length", 6)
+    })
+
+    it('Todos os usuários devem mostrar as informações pré-definidas', function () {
+      cy.get(paginaInicial.itensListaUsuarios).then(function (resposta) {
+        for (let item of resposta) {
+          cy.wrap(item).should("contain", "Nome")
+          cy.wrap(item).should("contain", "E-mail")
+          cy.wrap(item).find("#userDataDetalhe").should("have.text", "Ver detalhes")
+          cy.wrap(item).find("[data-test='userDataDelete']").should("exist")
+        }
+      })
+    })
+
+  })
+
+  describe('Recebendo resposta com 18 usuários', function () {
+    beforeEach(function () {
+      cy.intercept("GET", "api/v1/users", {
+        fixture: "get-usuarios/get-18-usuarios.json"
+      }).as("getUsers")
+
+      cy.wait("@getUsers")
+    })
+
+    it('Total de páginas deve ser 03', () => {
+      cy.get(paginaInicial.itemPaginacaoAtual).should("have.text", "1 de 3")
+    })
+
+    it('Botão de avançar página deve estar habilitado, e o de voltar deve estar desabilitado', function () {
+      cy.get(paginaInicial.buttonNextPage).should("be.enabled")
+      cy.get(paginaInicial.buttonPreviousPage).should("be.disabled")
+    })
+
+    it('Total de usuários mostrados na página deve ser até 06', function () {
+      cy.get(paginaInicial.itensListaUsuarios).should("have.length", 6)
     })
   })
+
+  describe('Recebendo resposta com 03 usuários', function () {
+    beforeEach(function () {
+      cy.intercept("GET", "api/v1/users", {
+        fixture: "get-usuarios/get-3-usuarios.json"
+      }).as("getUsers")
+
+      cy.wait("@getUsers")
+    })
+
+    it('Total de páginas deve ser 01', () => {
+      cy.get(paginaInicial.itemPaginacaoAtual).should("have.text", "1 de 1")
+    })
+
+    it('Botão de avançar página deve estar habilitado, e o de voltar deve estar desabilitado', function () {
+      cy.get(paginaInicial.buttonNextPage).should("be.disabled")
+      cy.get(paginaInicial.buttonPreviousPage).should("be.disabled")
+    })
+
+    it('Total de usuários mostrados na página deve ser 03', function () {
+      cy.get(paginaInicial.itensListaUsuarios).should("have.length", 3)
+    })
+  })
+
 })
--- cypress/support/pages/pagina-inicial.js
+++ cypress/support/pages/pagina-inicial.js
@@ -1,8 +1,12 @@
-export class paginaInicial {
+export class PaginaInicial {
     buttonNextPage = "#paginacaoProximo"
     buttonPreviousPage = "#paginacaoVoltar"
     buttonPreviousPage = "#paginacaoVoltar"
     buttonNovoUsuario = "[href='/users/novo']"
 
+    divListaUsuarios = "#listaUsuarios"
+    itensListaUsuarios = `${this.divListaUsuarios} > div`
+
     inputPesquisar = "[placeholder='E-mail ou nome']"
+    itemPaginacaoAtual = "#paginacaoAtual"
 }
