e20d66ac4b10d7f1199f76c97bfc3ba4c5de3685
luanpoppe <luanpoppe@gmail.com>
2024-04-28 16:37:51 -0300

    feat: adicionado testes validando o campo de nome ao cadastrar um novo usuário

--- cypress/e2e/criar-usuario.cy.js
+++ cypress/e2e/criar-usuario.cy.js
@@ -66,6 +66,63 @@ describe('Testes de criar usuário', function () {
     })
   })
 
+  describe('Cadastro com sucesso em casos limítrofes', function () {
+    afterEach(function () {
+      cy.deleteUserApi(user.id)
+    })
+
+    it('Nome com 100 caracteres é aceito', function () {
+      cy.intercept("POST", "/api/v1/users").as("criarUsuario")
+
+      let valorNome = ""
+      while (valorNome.length < 100) {
+        valorNome += "a"
+      }
+
+      cy.get(criarUsuario.inputEmail).type(userFaker.email)
+      cy.get(criarUsuario.inputNome).type(valorNome)
+
+      criarUsuario.clicarBotaoSalvar()
+      cy.wait("@criarUsuario").then(function (res) {
+        user = res.response.body
+        cy.contains("Usuário salvo com sucesso!").should("exist")
+      })
+    })
+
+    it('Nome com 4 caracteres é aceito', function () {
+      cy.intercept("POST", "/api/v1/users").as("criarUsuario")
+
+      const valorNome = "abcd"
+
+      cy.get(criarUsuario.inputEmail).type(userFaker.email)
+      cy.get(criarUsuario.inputNome).type(valorNome)
+
+      criarUsuario.clicarBotaoSalvar()
+      cy.wait("@criarUsuario").then(function (res) {
+        user = res.response.body
+        cy.contains("Usuário salvo com sucesso!").should("exist")
+      })
+    })
+
+    it('Email com 60 caracteres é aceito', function () {
+      cy.intercept("POST", "/api/v1/users").as("criarUsuario")
+
+      let valorEmail = "a@gmail.com."
+      while (valorEmail.length < 60) {
+        valorEmail += "a"
+      }
+
+      cy.get(criarUsuario.inputEmail).type(valorEmail)
+      cy.get(criarUsuario.inputNome).type(userFaker.name)
+
+      criarUsuario.clicarBotaoSalvar()
+      cy.wait("@criarUsuario").then(function (res) {
+        user = res.response.body
+        cy.contains("Usuário salvo com sucesso!").should("exist")
+      })
+    })
+  })
+
   describe("Tentativa de cadastro sem sucesso", function () {
     it('Campo de nome e de email vazios', function () {
       criarUsuario.clicarBotaoSalvar()
@@ -76,7 +133,7 @@ describe('Testes de criar usuário', function () {
     })
 
     describe.only('Validação do campo de nome', function () {
-      it('Campo de nome vazio', function () {
+      it('Campo de nome vazio não é aceito', function () {
         cy.get(criarUsuario.inputEmail).type(userFaker.email)
         criarUsuario.clicarBotaoSalvar()
 
@@ -86,7 +143,7 @@ describe('Testes de criar usuário', function () {
         cy.contains("Usuário salvo com sucesso!").should("not.exist")
       })
 
-      it('Nome com menos de 4 caracteres', function () {
+      it('Nome com menos de 4 caracteres não é aceito', function () {
         cy.get(criarUsuario.inputEmail).type(userFaker.email)
         cy.get(criarUsuario.inputNome).type("abc")
         criarUsuario.clicarBotaoSalvar()
@@ -96,9 +153,48 @@ describe('Testes de criar usuário', function () {
         cy.contains("O campo e-mail é obrigatório.").should("not.exist")
         cy.contains("Usuário salvo com sucesso!").should("not.exist")
       })
+
+      it('Nome com 100 ou mais caracteres não é aceito', function () {
+        let valorNome = ""
+        while (valorNome.length < 101) {
+          valorNome += "a"
+        }
+
+        cy.get(criarUsuario.inputEmail).type(userFaker.email)
+        cy.get(criarUsuario.inputNome).type(valorNome)
+        criarUsuario.clicarBotaoSalvar()
+
+        cy.contains("Informe no máximo 100 caracteres para o nome").should("exist")
+
+        cy.contains("O campo e-mail é obrigatório.").should("not.exist")
+        cy.contains("Usuário salvo com sucesso!").should("not.exist")
+      })
+
+      it('Nome contendo números não é aceito', function () {
+        cy.get(criarUsuario.inputEmail).type(userFaker.email)
+        cy.get(criarUsuario.inputNome).type("NomeUsuario123456")
+        criarUsuario.clicarBotaoSalvar()
+
+        cy.contains("Formato do nome é inválido.").should("exist")
+
+        cy.contains("O campo e-mail é obrigatório.").should("not.exist")
+        cy.contains("Usuário salvo com sucesso!").should("not.exist")
+      })
+
+      it('Nome contendo símbolos não é aceito', function () {
+        cy.get(criarUsuario.inputEmail).type(userFaker.email)
+        cy.get(criarUsuario.inputNome).type("NomeUsuario$")
+        criarUsuario.clicarBotaoSalvar()
+
+        cy.contains("Formato do nome é inválido.").should("exist")
+
+        cy.contains("O campo e-mail é obrigatório.").should("not.exist")
+        cy.contains("Usuário salvo com sucesso!").should("not.exist")
+      })
     })
 
     describe('Validação do campo de email', function () {
+
     })
   })
 
