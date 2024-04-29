2c09ed818c256d68b5f7b01e6094bf9b9c9d4324
luanpoppe <luanpoppe@gmail.com>
2024-04-28 21:40:08 -0300

    feat: adicionando teste de pesquisar pelo nome do usuário

--- cypress/e2e/pesquisar-usuarios.cy.js
+++ cypress/e2e/pesquisar-usuarios.cy.js
@@ -38,8 +38,8 @@ describe('Testes de pesquisa de usuários', function () {
     let user
 
     beforeEach(function () {
-      cy.createUserApi().then(function (resposta) {
-        user = resposta.body
+      cy.createUserApi().then(function (body) {
+        user = body
       })
     })
 
@@ -48,10 +48,33 @@ describe('Testes de pesquisa de usuários', function () {
     })
 
     it('Pesquisar por nome traz resultado esperado', function () {
-      cy.log('user.name', user.name)
-      paginaInicial.pesquisarUsuario(user.name).pause()
 
-      cy.wait("@getPesquisa")
+      // Se o resultado da pesquisa trouxer 6 ou menos usuários, todos aparecerão na página, e assim busco nos elementos da página. Este será o cenário mais comum.
+      // Se por acaso o resultado trouxer mais de 6 usuários, o usuário criado por estar em outra página que não a primeira, tornando inviável buscar página a página.
+      // Nesse segundo cenário, a solução que encontrei foi checar que o body da resposta da API traz o usuário criado na sua lista de usuários pesquisados.
+      paginaInicial.pesquisarUsuario(user.name)
+      cy.wait("@getPesquisa").then(function (res) {
+        cy.get(paginaInicial.itensListaUsuarios).should("have.length.above", 0)
+
+        const usuariosPesquisados = res.response.body
+
+        if (usuariosPesquisados.length <= 6) {
+          cy.get(paginaInicial.itensListaUsuarios).should("contain.text", user.name)
+
+          // Garantindo que mesmo que o email seja muito grande e não apareça todo na tela, a parte que aparece é uma "sub-parte" do email criado
+          cy.get('[data-test="userDataEmail"]').invoke("text").then((texto) => {
+            const emailsSeparados = texto.split("E-mail: ").toString().split("...").toString().split(",")
+            const isEmailInPage = emailsSeparados.some((item) => user.name.includes(item))
+
+            expect(isEmailInPage).to.equal(true)
+          })
+        }
+        else {
+          const isUserCreatedInResponse = usuariosPesquisados.some((usuario) => usuario.name == user.name)
+          expect(isUserCreatedInResponse).to.equal(true)
+          cy.get(paginaInicial.buttonNextPage).should("be.enabled")
+        }
+      })
     })
   })
 
